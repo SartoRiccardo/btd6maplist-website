26e4d40047ee0f8b51a78e9f3a4ec69d8550cc22
SartoRiccardo <rikki.sarto@gmail.com>
2024-08-08 13:54:39 +0200

    feat: Added inter font

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,17 +3,17 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
-import { luckiestGuy } from "@/lib/fonts";
+import { luckiestGuy, inter } from "@/lib/fonts";
 
 export const metadata = {
-  title: "BTD6 Maplist",
-  description: "A community curated list of the best BTD6 custom maps",
+  title: "Bloons TD 6 Maplist",
+  description: "A community curated list of the best Bloons TD 6 custom maps",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${luckiestGuy.variable}`}>
+      <body className={`${luckiestGuy.variable} ${inter.variable}`}>
         <div className={`content`}>
           <Header />
           {children}
--- src/lib/fonts.js
+++ src/lib/fonts.js
@@ -1,7 +1,13 @@
-import { Luckiest_Guy } from "next/font/google";
+import { Luckiest_Guy, Inter } from "next/font/google";
 
 export const luckiestGuy = Luckiest_Guy({
   weight: "400",
   subsets: ["latin"],
   variable: "--font-luckiest-guy",
+});
+
+export const inter = Inter({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-inter",
 });
