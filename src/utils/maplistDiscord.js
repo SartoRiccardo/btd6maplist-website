cc3f973aa9daff763ad8be74acf408cbfa12c124
SartoRiccardo <rikki.sarto@gmail.com>
2024-08-19 15:39:32 +0200

    fix: Updated mod role IDs

--- src/utils/maplistDiscord.js
+++ src/utils/maplistDiscord.js
@@ -1,3 +1,3 @@
 export const maplistGuild = "1162188507800944761";
 export const maplistModRole = "1162192791716237322";
-export const explistModRole = maplistModRole;
+export const explistModRole = "1256145562516979813";
