031b9638c72be694e75a3399bd90c108567c40dd
SartoRiccardo <rikki.sarto@gmail.com>
2024-08-19 19:48:25 +0200

    misc: Addede title as metadata

---
+++ src/app/config/page.client.js
@@ -0,0 +1,127 @@
+"use client";
+import { selectDiscordAccessToken } from "@/features/authSlice";
+import { selectMaplistConfig, setConfig } from "@/features/maplistSlice";
+import { useAppSelector } from "@/lib/store";
+import { editConfig } from "@/server/maplistRequests.client";
+import { revalidateLeaderboard } from "@/server/revalidations";
+import { isFloat, isInt } from "@/utils/functions";
+import { Formik } from "formik";
+import { Fragment } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+
+const configNames = {
+  points_top_map: "Points for the #1 map",
+  points_bottom_map: "Points for the last map",
+  formula_slope: "Formula slope",
+  points_extra_lcc: "Extra points for LCCs",
+  points_multi_gerry: "No Geraldo point multiplier",
+  points_multi_bb: "Black Border point multiplier",
+  decimal_digits: "Decimal digits to round to",
+  map_count: "Number of maps on the list",
+  current_btd6_ver: "Current BTD6 version",
+};
+
+const intFields = ["decimal_digits", "map_count"];
+const floatFields = [
+  "points_top_map",
+  "points_bottom_map",
+  "formula_slope",
+  "points_extra_lcc",
+  "points_multi_gerry",
+  "points_multi_bb",
+  "current_btd6_ver",
+];
+
+export default function ConfigVarPage_C() {
+  const config = useAppSelector(selectMaplistConfig);
+  const accessToken = useAppSelector(selectDiscordAccessToken);
+  const dispatch = useDispatch();
+
+  const defaultVals = {
+    ...config,
+    current_btd6_ver: config.current_btd6_ver / 10,
+  };
+
+  const validate = (values) => {
+    const errors = {};
+    for (const key of Object.keys(values)) {
+      if (intFields.includes(key)) {
+        if (!isInt(values[key])) errors[key] = "This must be an integer number";
+      } else if (floatFields.includes(key)) {
+        if (!isFloat(values[key])) errors[key] = "This must be a number";
+      }
+    }
+    return errors;
+  };
+
+  const handleSubmit = async (values, { setErrors }) => {
+    values = {
+      ...values,
+      formula_slope: values.formula_slope.toString(),
+      current_btd6_ver: Math.floor(values.current_btd6_ver * 10).toString(),
+    };
+    const { errors, data } = await editConfig(accessToken.access_token, values);
+    if (Object.keys(errors).length) {
+      setErrors(errors);
+      return;
+    }
+    revalidateLeaderboard();
+    dispatch(setConfig({ config: data }));
+  };
+
+  return (
+    <>
+      <h1 className="text-center">Config Variables</h1>
+
+      <Formik
+        onSubmit={handleSubmit}
+        validate={validate}
+        initialValues={defaultVals}
+      >
+        {({ handleSubmit, handleChange, values, errors, isSubmitting }) => (
+          <Form onSubmit={handleSubmit}>
+            <div className="panel panel-container">
+              <div className="row flex-row-space">
+                {Object.keys(config)
+                  .sort()
+                  .map((key) => (
+                    <Fragment key={key}>
+                      <div className="col-5 col-sm-6">
+                        <p>{configNames[key]}</p>
+                      </div>
+                      <div className="col-7 col-sm-6">
+                        <Form.Group>
+                          <Form.Control
+                            name={key}
+                            type="text"
+                            placeholder={defaultVals[key]}
+                            value={values[key]}
+                            onChange={handleChange}
+                            isInvalid={
+                              values[key].length === 0 || key in errors
+                            }
+                            disabled={isSubmitting}
+                            autoComplete="off"
+                          />
+                          <Form.Control.Feedback type="invalid">
+                            {errors[key]}
+                          </Form.Control.Feedback>
+                        </Form.Group>
+                      </div>
+                    </Fragment>
+                  ))}
+              </div>
+            </div>
+
+            <div className="d-flex flex-col-space justify-content-center">
+              <Button disabled={isSubmitting} type="submit">
+                Save
+              </Button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
+}
---
+++ src/app/user/edit/page.client.js
@@ -0,0 +1,200 @@
+"use client";
+import "./useredit.css";
+import {
+  selectDiscordAccessToken,
+  selectMaplistProfile,
+  setBtd6Profile,
+  setMinMaplistProfile,
+} from "@/features/authSlice";
+import { useAppDispatch, useAppSelector } from "@/lib/store";
+import { Button, Collapse, Form } from "react-bootstrap";
+import { Formik } from "formik";
+import { editProfile } from "@/server/maplistRequests.client";
+import { getBtd6User } from "@/server/ninjakiwiRequests";
+import { revalidateUser } from "@/server/revalidations";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function EditSelf_C() {
+  const { maplistProfile } = useAppSelector(selectMaplistProfile);
+  const { access_token } = useAppSelector(selectDiscordAccessToken);
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+  const [showOakHelp, setShowOakHelp] = useState(false);
+
+  const validate = async (values) => {
+    const errors = {};
+    if (!values.name.length) errors.name = "Name cannot be blank";
+    if (values.oak.length) {
+      if (!values.oak.startsWith("oak_"))
+        errors.oak = (
+          <>
+            OAK must start with <span className="mono">oak_...</span>
+          </>
+        );
+      else if (values.oak.length !== 24)
+        errors.oak = `OAK must be exactly 24 characters long (yours is ${values.oak.length})`;
+    }
+
+    return errors;
+  };
+
+  const handleSubmit = async (values, { setErrors }) => {
+    const result = await editProfile(access_token, maplistProfile.id, values);
+
+    if (Object.keys(result.errors).length) {
+      setErrors(result.errors);
+      return;
+    }
+
+    revalidateUser(maplistProfile.id);
+    if (values.oak.length && values.oak !== maplistProfile.oak) {
+      const btd6Profile = await getBtd6User(values.oak);
+      dispatch(setBtd6Profile({ btd6Profile }));
+    }
+    dispatch(setMinMaplistProfile({ ...result.data }));
+  };
+
+  return (
+    <>
+      <h1 className="text-center">Edit Profile</h1>
+
+      <Formik
+        onSubmit={handleSubmit}
+        validate={validate}
+        initialValues={{
+          name: maplistProfile.name,
+          oak: maplistProfile.oak || "",
+        }}
+      >
+        {({ handleSubmit, handleChange, values, errors, isSubmitting }) => (
+          <Form noValidate onSubmit={handleSubmit}>
+            <div className="panel panel-container">
+              <div className="row flex-row-space">
+                <div className="col-5 col-sm-6">
+                  <p>Username</p>
+                </div>
+                <div className="col-7 col-sm-6">
+                  <Form.Group>
+                    <Form.Control
+                      name="name"
+                      type="text"
+                      placeholder="chimenea.mo"
+                      value={values.name}
+                      onChange={handleChange}
+                      isInvalid={"name" in errors}
+                      disabled={isSubmitting}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      {errors.name}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </div>
+
+                <div className="col-5 col-sm-6">
+                  <p>NinjaKiwi OAK</p>
+                </div>
+                <div className="col-7 col-sm-6">
+                  <Form.Group>
+                    <Form.Control
+                      type="text"
+                      name="oak"
+                      placeholder="oak_b1B..."
+                      value={values.oak}
+                      onChange={handleChange}
+                      isInvalid={"oak" in errors}
+                      disabled={isSubmitting}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      {errors.oak}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </div>
+                <div className="col-12">
+                  <p className="muted text-center mb-2">
+                    <a
+                      href="https://support.ninjakiwi.com/hc/en-us/articles/13438499873937-Open-Data-API"
+                      target="_blank"
+                    >
+                      NinjaKiwi OAKs &nbsp;
+                      <i className="bi bi-box-arrow-up-right" />
+                    </a>{" "}
+                    (OpenData API Keys) are used to give you your in-game
+                    profile picture and banner. You can generate one inside of
+                    BTD6 very easily.
+                  </p>
+                  <div className="d-flex justify-content-center">
+                    <Button
+                      onClick={(_e) => setShowOakHelp(!showOakHelp)}
+                      className="fs-6 mb-3"
+                    >
+                      How do I get mine?
+                    </Button>
+                  </div>
+                  <Collapse in={showOakHelp}>
+                    <div>
+                      <p className="text-center">
+                        Open BTD6 and go into the settings, then click{" "}
+                        <b>
+                          <u>Account</u>
+                        </b>
+                      </p>
+                      <div className="d-flex justify-content-center">
+                        <img
+                          src="https://i.imgur.com/FurlzfB.png"
+                          className="mb-3 opendata-guide"
+                        />
+                      </div>
+
+                      <p className="text-center">
+                        Click{" "}
+                        <b>
+                          <u>Open Data API</u>
+                        </b>{" "}
+                        on the bottom right
+                      </p>
+                      <div className="d-flex justify-content-center">
+                        <img
+                          src="https://i.imgur.com/tCqlDYj.png"
+                          className="mb-3 opendata-guide"
+                        />
+                      </div>
+
+                      <p className="text-center">
+                        Click{" "}
+                        <b>
+                          <u>Generate New Key</u>
+                        </b>{" "}
+                        and then you can copy it and you're done!
+                      </p>
+                      <div className="d-flex justify-content-center">
+                        <img
+                          src="https://i.imgur.com/owhXl4U.png"
+                          className="mb-3 opendata-guide"
+                        />
+                      </div>
+                    </div>
+                  </Collapse>
+                </div>
+              </div>
+            </div>
+
+            <div className="d-flex flex-col-space justify-content-center">
+              <Button
+                disabled={isSubmitting}
+                onClick={(_e) =>
+                  router.push(`/user/${maplistProfile.id}`, { scroll: false })
+                }
+              >
+                Back
+              </Button>
+              <Button disabled={isSubmitting} type="submit">
+                Save
+              </Button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
+}
