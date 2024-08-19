d2f06602ec971443a1e1b62fdab7db3e244ebf09
SartoRiccardo <rikki.sarto@gmail.com>
2024-08-20 01:58:10 +0200

    misc: Formatted code into a component

--- src/app/map/add/page.client.js
+++ src/app/map/add/page.client.js
@@ -2,7 +2,7 @@
 import "./newmap.css";
 import { getCustomMap } from "@/server/ninjakiwiRequests";
 import { Formik } from "formik";
-import { useState } from "react";
+import { createContext, useContext, useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import { useAppSelector } from "@/lib/store";
 import { selectMaplistProfile } from "@/features/authSlice";
@@ -11,6 +11,8 @@ import { difficulties } from "@/utils/maplistUtils";
 
 const MAX_NAME_LEN = 100;
 const MAX_URL_LEN = 300;
+
+const FormikContext = createContext({});
 
 export default function MapForm_C({ initialValues, code }) {
   const [currentMap, setCurrentMap] = useState(
@@ -84,134 +86,116 @@ export default function MapForm_C({ initialValues, code }) {
           r6_start: "",
           map_data: "",
           map_data_req_permission: false,
+          creators: [{ id: 0, role: "" }],
+          validators: [{ id: 0, version: "" }],
+          additional_codes: [{ code: "", description: "" }],
+          version_compatibilities: [{ version: "", status: 0 }],
         }
       }
       onSubmit={handleSubmit}
     >
-      {({ handleSubmit, handleChange, values, errors, isSubmitting }) => {
+      {(formikProps) => {
+        const {
+          handleSubmit,
+          handleChange,
+          handleBlur,
+          values,
+          touched,
+          errors,
+          isSubmitting,
+        } = formikProps;
+
         return (
-          <Form onSubmit={handleSubmit} key={0}>
-            <Form.Group className="mapcode-input">
-              <Form.Label>Map Code</Form.Label>
-              <Form.Control
-                name="code"
-                type="text"
-                placeholder={"CHIMENE"}
-                value={values.code}
-                onChange={handleChange}
-                isInvalid={values.code.length === 0 || "code" in errors}
-                isValid={!("code" in errors)}
-                disabled={isSubmitting}
-                autoComplete="off"
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.code}
-              </Form.Control.Feedback>
-            </Form.Group>
+          <FormikContext.Provider value={formikProps}>
+            <Form onSubmit={handleSubmit} key={0}>
+              <Form.Group className="mapcode-input">
+                <Form.Label>Map Code</Form.Label>
+                <Form.Control
+                  name="code"
+                  type="text"
+                  placeholder={"CHIMENE"}
+                  value={values.code}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  isInvalid={
+                    touched.code &&
+                    (values.code.length === 0 || "code" in errors)
+                  }
+                  isValid={!("code" in errors)}
+                  disabled={isSubmitting}
+                  autoComplete="off"
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.code}
+                </Form.Control.Feedback>
+              </Form.Group>
 
-            <p className="muted text-center">
-              You can also paste the whole map share URL
-            </p>
+              <p className="muted text-center">
+                You can also paste the whole map share URL
+              </p>
 
-            {currentMap && currentMap.valid && (
-              <>
-                <hr className="mb-5" />
+              {currentMap && currentMap.valid && (
+                <>
+                  <hr className="mb-5" />
 
-                <div className="row flex-row-space mt-5">
-                  <div className="col-12 col-lg-6">
-                    <div className="panel pt-3 px-3 pb-4 map-preview">
-                      <div className="map-name-container">
-                        <Form.Group>
-                          <Form.Control
-                            name="name"
-                            type="text"
-                            placeholder={"chimene.mo's map"}
-                            value={values.name}
-                            onChange={handleChange}
-                            isInvalid={
-                              values.name.length === 0 || "name" in errors
-                            }
-                            isValid={!("name" in errors)}
-                            disabled={isSubmitting}
-                            autoComplete="off"
-                          />
-                        </Form.Group>
+                  <div className="row flex-row-space mt-5">
+                    <div className="col-12 col-lg-6">
+                      <div className="panel pt-3 px-3 pb-4 map-preview">
+                        <div className="map-name-container">
+                          <Form.Group>
+                            <Form.Control
+                              name="name"
+                              type="text"
+                              placeholder={"chimene.mo's map"}
+                              value={values.name}
+                              onChange={handleChange}
+                              isInvalid={
+                                touched.name &&
+                                (values.name.length === 0 || "name" in errors)
+                              }
+                              isValid={!("name" in errors)}
+                              disabled={isSubmitting}
+                              autoComplete="off"
+                            />
+                          </Form.Group>
+                        </div>
+
+                        <img
+                          className="w-100"
+                          src={`https://data.ninjakiwi.com/btd6/maps/map/${currentMap.code}/preview`}
+                        />
                       </div>
-
-                      <img
-                        className="w-100"
-                        src={`https://data.ninjakiwi.com/btd6/maps/map/${currentMap.code}/preview`}
-                      />
                     </div>
-                  </div>
 
-                  <div className="col-12 col-lg-6">
-                    <div className="panel h-100">
-                      <div className="row flex-row-space my-3">
-                        {maplistProfile.roles.includes(
-                          process.env.NEXT_PUBLIC_LISTMOD_ROLE
-                        ) && (
-                          <>
-                            <div className="col-6 col-lg-7">
-                              <p>List Placement</p>
-                            </div>
-                            <div className="col-6 col-lg-5">
-                              <Form.Group>
-                                <Form.Control
-                                  name="placement_curver"
-                                  type="number"
-                                  placeholder="N/A"
-                                  value={values.placement_curver}
-                                  onChange={handleChange}
-                                  isInvalid={"placement_curver" in errors}
-                                  isValid={
-                                    !("placement_curver" in errors) &&
-                                    values.placement_curver
-                                  }
-                                  disabled={isSubmitting}
-                                  autoComplete="off"
-                                />
-                                <Form.Control.Feedback type="invalid">
-                                  {errors.placement_curver}
-                                </Form.Control.Feedback>
-                              </Form.Group>
-                            </div>
+                    <div className="col-12 col-lg-6">
+                      <div className="panel h-100">
+                        <div className="row flex-row-space my-3">
+                          {maplistProfile.roles.includes(
+                            process.env.NEXT_PUBLIC_LISTMOD_ROLE
+                          ) && (
+                            <>
+                              <SidebarField
+                                title="List Placement"
+                                name="placement_curver"
+                                type="number"
+                                placeholder="N/A"
+                                invalidFeedback
+                              />
 
-                            <div className="col-6 col-lg-7">
-                              <p>List Placement (All Versions)</p>
-                            </div>
-                            <div className="col-6 col-lg-5">
-                              <Form.Group>
-                                <Form.Control
-                                  name="placement_allver"
-                                  type="number"
-                                  placeholder="N/A"
-                                  value={values.placement_allver}
-                                  onChange={handleChange}
-                                  isInvalid={"placement_allver" in errors}
-                                  isValid={
-                                    !("placement_allver" in errors) &&
-                                    values.placement_allver
-                                  }
-                                  disabled={isSubmitting}
-                                  autoComplete="off"
-                                />
-                                <Form.Control.Feedback type="invalid">
-                                  {errors.placement_allver}
-                                </Form.Control.Feedback>
-                              </Form.Group>
-                            </div>
-                          </>
-                        )}
+                              <SidebarField
+                                title="List Placement (All Versions)"
+                                name="placement_allver"
+                                type="number"
+                                placeholder="N/A"
+                                invalidFeedback
+                              />
+                            </>
+                          )}
 
-                        {maplistProfile.roles.includes(
-                          process.env.NEXT_PUBLIC_EXPMOD_ROLE
-                        ) && (
-                          <>
-                            <div className="col-6 col-lg-7">
-                              <p>Expert Difficulty</p>
-                            </div>
-                            <div className="col-6 col-lg-5">
+                          {maplistProfile.roles.includes(
+                            process.env.NEXT_PUBLIC_EXPMOD_ROLE
+                          ) && (
+                            <SidebarField title="Expert Difficulty">
                               <Form.Select
                                 name="difficulty"
                                 value={values.difficulty}
@@ -225,97 +209,125 @@ export default function MapForm_C({ initialValues, code }) {
                                   </option>
                                 ))}
                               </Form.Select>
-                            </div>
-                          </>
-                        )}
+                            </SidebarField>
+                          )}
 
-                        <div className="col-6 col-lg-7">
-                          <p>Map Data</p>
-                        </div>
-                        <div className="col-6 col-lg-5">
-                          <Form.Group>
-                            <Form.Control
-                              name="map_data"
-                              type="text"
-                              placeholder="https://drive.google.com/..."
-                              value={
-                                values.map_data_req_permission
-                                  ? ""
-                                  : values.map_data
-                              }
-                              onChange={handleChange}
-                              isInvalid={"map_data" in errors}
-                              isValid={
-                                !("map_data" in errors) &&
-                                values.map_data &&
-                                !values.map_data_req_permission
-                              }
-                              disabled={
-                                isSubmitting || values.map_data_req_permission
-                              }
-                              autoComplete="off"
-                            />
-                            <Form.Control.Feedback type="invalid">
-                              {errors.placement_curver}
-                            </Form.Control.Feedback>
-                          </Form.Group>
+                          <SidebarField
+                            title="Map Data"
+                            name="map_data"
+                            placeholder="https://drive.google.com/..."
+                            invalidFeedback
+                            appendChildren
+                            value={({ values }) =>
+                              values.map_data_req_permission
+                                ? ""
+                                : values.map_data
+                            }
+                            isValid={({ values }) =>
+                              !("map_data" in errors) &&
+                              values.map_data &&
+                              !values.map_data_req_permission
+                            }
+                            disabled={({ isSubmitting, values }) =>
+                              isSubmitting || values.map_data_req_permission
+                            }
+                          >
+                            <Form.Group className="mb-3">
+                              <Form.Check
+                                name="map_data_req_permission"
+                                checked={values.map_data_req_permission}
+                                onChange={handleChange}
+                                label="Should ask permission to the creator"
+                                disabled={isSubmitting}
+                              />
+                            </Form.Group>
+                          </SidebarField>
 
-                          <Form.Group className="mb-3">
-                            <Form.Check
-                              name="map_data_req_permission"
-                              checked={values.map_data_req_permission}
-                              onChange={handleChange}
-                              label="Should ask permission to the creator"
-                              disabled={isSubmitting}
-                            />
-                          </Form.Group>
+                          <SidebarField
+                            title="Round 6 start image/video"
+                            name="r6_start"
+                            placeholder="https://imgur.com/..."
+                            invalidFeedback
+                          />
+                          <p className="muted text-center">
+                            Please don't upload Discord URLs. Upload the image
+                            to{" "}
+                            <a href="https://imgur.com/" target="_blank">
+                              Imgur
+                            </a>{" "}
+                            first or another site like that.
+                          </p>
                         </div>
-
-                        <div className="col-6 col-lg-7">
-                          <p>Round 6 start image/video</p>
-                        </div>
-                        <div className="col-6 col-lg-5">
-                          <Form.Group>
-                            <Form.Control
-                              name="r6_start"
-                              type="text"
-                              placeholder="https://imgur.com/..."
-                              value={values.r6_start}
-                              onChange={handleChange}
-                              isInvalid={"r6_start" in errors}
-                              isValid={
-                                !("r6_start" in errors) && values.r6_start
-                              }
-                              disabled={isSubmitting}
-                              autoComplete="off"
-                            />
-                            <Form.Control.Feedback type="invalid">
-                              {errors.r6_start}
-                            </Form.Control.Feedback>
-                          </Form.Group>
-                        </div>
-                        <p className="muted text-center">
-                          Please don't upload Discord URLs. Upload the image to{" "}
-                          <a href="https://imgur.com/" target="_blank">
-                            Imgur
-                          </a>{" "}
-                          first or another site like that.
-                        </p>
                       </div>
                     </div>
                   </div>
-                </div>
 
-                <div className="flex-hcenter mt-5">
-                  <Button type="submit" disabled={isSubmitting}>
-                    Submit
-                  </Button>
-                </div>
-              </>
-            )}
-          </Form>
+                  <div className="flex-hcenter mt-5">
+                    <Button type="submit" disabled={isSubmitting}>
+                      Submit
+                    </Button>
+                  </div>
+                </>
+              )}
+            </Form>
+          </FormikContext.Provider>
         );
       }}
     </Formik>
+  );
+}
+
+function SidebarField({
+  title,
+  name,
+  type,
+  placeholder,
+  invalidFeedback,
+  children,
+  appendChildren,
+  value,
+  isValid,
+  disabled,
+}) {
+  const formikProps = useContext(FormikContext);
+  const { handleChange, handleBlur, values, touched, errors, isSubmitting } =
+    formikProps;
+
+  return (
+    <>
+      <div className="col-6 col-lg-7">
+        <p>{title}</p>
+      </div>
+      <div className="col-6 col-lg-5">
+        {children && !appendChildren ? (
+          children
+        ) : (
+          <Form.Group>
+            <Form.Control
+              name={name}
+              type={type || "text"}
+              placeholder={placeholder || ""}
+              value={value ? value(formikProps) : values[name]}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              isInvalid={touched[name] && [name] in errors}
+              isValid={
+                isValid
+                  ? isValid(formikProps)
+                  : !([name] in errors) && values[name]
+              }
+              disabled={disabled ? disabled(formikProps) : isSubmitting}
+              autoComplete="off"
+            />
+            {invalidFeedback && (
+              <Form.Control.Feedback type="invalid">
+                {errors[name]}
+              </Form.Control.Feedback>
+            )}
+            {appendChildren && children}
+          </Form.Group>
+        )}
+      </div>
+    </>
   );
 }
