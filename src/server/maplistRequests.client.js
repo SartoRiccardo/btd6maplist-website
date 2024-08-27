da6e66b0424db69313a47bd6097b53fdb2687249
SartoRiccardo <rikki.sarto@gmail.com>
2024-08-27 14:47:54 +0200

    feat: API Discord auth is via Authorization header

--- src/server/maplistRequests.client.js
+++ src/server/maplistRequests.client.js
@@ -1,7 +1,11 @@
 export async function editProfile(token, userId, profile) {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/users/${userId}`,
-    { method: "PUT", body: JSON.stringify({ token, ...profile }) }
+    {
+      method: "PUT",
+      headers: { Authorization: `Bearer ${token}` },
+      body: JSON.stringify({ token, ...profile }),
+    }
   );
   return await response.json();
 }
@@ -9,8 +13,21 @@ export async function editProfile(token, userId, profile) {
 export async function editConfig(token, config) {
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/config`, {
     method: "PUT",
+    headers: { Authorization: `Bearer ${token}` },
     body: JSON.stringify({ token, config }),
   });
-  if (response.status === 401) return null;
+  if (response.status === 401)
+    return { errors: { "": "Unauthorized", data: {} } };
+  return await response.json();
+}
+
+export async function addMap(token, map) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/maps`, {
+    method: "POST",
+    headers: { Authorization: `Bearer ${token}` },
+    body: JSON.stringify({ token, map }),
+  });
+  if (response.status === 401)
+    return { errors: { "": "Unauthorized", data: {} } };
   return await response.json();
 }
