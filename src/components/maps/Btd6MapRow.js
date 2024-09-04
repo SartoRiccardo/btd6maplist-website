56ca29a264f4a684c0c643bc28db9e6ebff26938
SartoRiccardo <rikki.sarto@gmail.com>
2024-09-05 00:12:00 +0200

    fix: Hydration error - illegal nesting

--- src/components/maps/Btd6MapRow.js
+++ src/components/maps/Btd6MapRow.js
@@ -6,20 +6,34 @@ export default function Btd6MapRow({ map, hrefBase, completion }) {
   completion = completion instanceof Array ? completion : [completion];
 
   const cmpMap = (
+    <div className="d-flex align-self-center">
+      <img
+        className={`btd6mapImage`}
+        src={`https://data.ninjakiwi.com/btd6/maps/map/${map.code}/preview`}
+      />
+      <div className="d-flex flex-column justify-content-center">
+        <p className={`mb-0 ps-3 ${btd6Font.className} font-border fs-5`}>
+          {map.name}
+        </p>
+      </div>
+    </div>
+  );
+
+  return (
     <div className={`panel py-2 my-2 btd6mapRow`}>
       <div className="row">
         <div className="col-12 col-md-5 col-lg-7 d-flex pb-3 pb-md-0">
-          <div className="d-flex align-self-center">
-            <img
-              className={`btd6mapImage`}
-              src={`https://data.ninjakiwi.com/btd6/maps/map/${map.code}/preview`}
-            />
-            <div className="d-flex flex-column justify-content-center">
-              <p className={`mb-0 ps-3 ${btd6Font.className} font-border fs-5`}>
-                {map.name}
-              </p>
-            </div>
-          </div>
+          {hrefBase ? (
+            <Link
+              scroll={false}
+              className="btd6map-clickable"
+              href={`${hrefBase}/${map.code}`}
+            >
+              {cmpMap}
+            </Link>
+          ) : (
+            cmpMap
+          )}
         </div>
 
         <div className="col-12 col-md-7 col-lg-5 align-self-center">
@@ -31,17 +45,5 @@ export default function Btd6MapRow({ map, hrefBase, completion }) {
         </div>
       </div>
     </div>
-  );
-
-  return hrefBase ? (
-    <Link
-      scroll={false}
-      className={"btd6map-clickable"}
-      href={`${hrefBase}/${map.code}`}
-    >
-      {cmpMap}
-    </Link>
-  ) : (
-    cmpMap
   );
 }
