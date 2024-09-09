fe4162009dcb872e360305090c0674ecc2e1d621
SartoRiccardo <rikki.sarto@gmail.com>
2024-09-09 22:47:48 +0200

    fix: useIsWindows should correctly only do its job client side

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -5,11 +5,16 @@ import {
 } from "@/features/authSlice";
 import { selectMaplistConfig } from "@/features/maplistSlice";
 import { useAppSelector } from "@/lib/store";
+import { useEffect, useState } from "react";
 
 export const useMaplistConfig = () => useAppSelector(selectMaplistConfig);
 export const useDiscordToken = () => useAppSelector(selectDiscordAccessToken);
 export const useAuthLevels = () => useAppSelector(selectAuthLevels);
 export const useIsWindows = () => {
-  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-  return /Windows/.test(userAgent);
+  const [isWindows, setIsWindows] = useState(false);
+  useEffect(() => {
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    if (userAgent) setIsWindows(/Windows/.test(userAgent));
+  }, []);
+  return isWindows;
 };
