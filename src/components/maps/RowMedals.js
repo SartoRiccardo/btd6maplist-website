59946fc6f261722d8647af653028fd343eb24f48
SartoRiccardo <rikki.sarto@gmail.com>
2024-09-11 22:22:35 +0200

    feat: Medal description on hover

--- src/components/maps/RowMedals.js
+++ src/components/maps/RowMedals.js
@@ -1,24 +1,53 @@
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+
 const medal_size = 45;
 export default function RowMedals({ black_border, no_geraldo, current_lcc }) {
   return (
     <div>
-      <img
-        src={black_border ? "/medals/medal_bb.webp" : "/medals/medal_win.webp"}
-        width={medal_size}
-        height={medal_size}
-      />
-      <img
-        src="/medals/medal_nogerry.webp"
-        width={medal_size}
-        height={medal_size}
-        className={`${!no_geraldo ? "comp-blocked" : ""} mx-2`}
-      />
-      <img
-        src="/medals/medal_lcc.webp"
-        width={medal_size}
-        height={medal_size}
-        className={!current_lcc ? "transparent" : ""}
-      />
+      <OverlayTrigger
+        overlay={(props) => (
+          <Tooltip {...props}>
+            CHIMPS completion{" "}
+            {black_border && (
+              <>
+                <br />
+                (Black Border)
+              </>
+            )}
+          </Tooltip>
+        )}
+      >
+        <img
+          src={
+            black_border ? "/medals/medal_bb.webp" : "/medals/medal_win.webp"
+          }
+          width={medal_size}
+          height={medal_size}
+        />
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        overlay={(props) => <Tooltip {...props}>No Optimal Hero</Tooltip>}
+      >
+        <img
+          src="/medals/medal_nogerry.webp"
+          width={medal_size}
+          height={medal_size}
+          className={`${!no_geraldo ? "comp-blocked" : ""} mx-2`}
+        />
+      </OverlayTrigger>
+
+      {current_lcc && (
+        <OverlayTrigger
+          overlay={(props) => <Tooltip {...props}>Current LCC</Tooltip>}
+        >
+          <img
+            src="/medals/medal_lcc.webp"
+            width={medal_size}
+            height={medal_size}
+          />
+        </OverlayTrigger>
+      )}
     </div>
   );
 }
