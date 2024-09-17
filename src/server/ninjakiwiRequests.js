53e6af539a1cf76dd8faf3345771ab860e7ec0d0
SartoRiccardo <rikki.sarto@gmail.com>
2024-09-17 14:47:47 +0200

    fix: Cache is not defined

--- src/server/ninjakiwiRequests.js
+++ src/server/ninjakiwiRequests.js
@@ -2,7 +2,6 @@ const BASE_URL = "https://data.ninjakiwi.com";
 
 export async function getBtd6User(oakOrUid) {
   const response = await fetch(`${BASE_URL}/btd6/users/${oakOrUid}`, {
-    cache,
     next: { revalidate: 3600 * 24 },
   });
   return response.status === 200 ? (await response.json()).body : null;
@@ -10,7 +9,6 @@ export async function getBtd6User(oakOrUid) {
 
 export async function getCustomMap(code) {
   const response = await fetch(`${BASE_URL}/btd6/maps/map/${code}`, {
-    cache,
     next: { revalidate: 3600 * 24 },
   });
   return response.status === 200 ? (await response.json()).body : null;
