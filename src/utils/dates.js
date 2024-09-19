063e25254b4f4a9a368f64b31ead8f85c77efe7d
SartoRiccardo <rikki.sarto@gmail.com>
2024-09-20 00:18:00 +0200

    fix: Plural

--- src/utils/dates.js
+++ src/utils/dates.js
@@ -4,10 +4,10 @@ export function fromNow(timestamp) {
 
   const seconds = relative % 60;
   const minutes = Math.floor(relative / 60);
-  if (minutes === 0) return `${seconds} seconds ago`;
+  if (minutes === 0) return `${seconds} second${seconds > 1 ? "s" : ""} ago`;
   const hours = Math.floor(relative / (60 * 60));
-  if (hours === 0) return `${minutes} minutes ago`;
+  if (hours === 0) return `${minutes} minute${minutes > 1 ? "s" : ""} ago`;
   const days = Math.floor(relative / (60 * 60 * 24));
-  if (days === 0) return `${hours} hours ago`;
-  return `${days} days ago`;
+  if (days === 0) return `${hours} hour${hours > 1 ? "s" : ""} ago`;
+  return `${days} day${days > 1 ? "s" : ""} ago`;
 }
