60937d19a809723304b6e80383d6b7e7fab7292b
SartoRiccardo <rikki.sarto@gmail.com>
2024-10-08 19:51:55 +0200

    feat: Changed runtimeCaching slightly

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,11 @@
 // https://ducanh-next-pwa.vercel.app/docs/next-pwa/getting-started
 import withPWAInit from "@ducanh2912/next-pwa";
 
-const defaultSwCache = [
+const nextConfig = {
+  output: "standalone",
+};
+
+const runtimeCaching = [
   {
     urlPattern: /^https:\/\/fonts\.(?:gstatic)\.com\/.*/i,
     handler: "CacheFirst",
@@ -63,32 +67,6 @@ const defaultSwCache = [
     },
   },
 
-  //   {
-  //     urlPattern: /\.(?:mp3|wav|ogg)$/i,
-  //     handler: "CacheFirst",
-  //     options: {
-  //       rangeRequests: true,
-  //       cacheName: "static-audio-assets",
-  //       expiration: {
-  //         maxEntries: 32,
-  //         maxAgeSeconds: 24 * 60 * 60, // 24 hours
-  //       },
-  //     },
-  //   },
-
-  //   {
-  //     urlPattern: /\.(?:mp4)$/i,
-  //     handler: "CacheFirst",
-  //     options: {
-  //       rangeRequests: true,
-  //       cacheName: "static-video-assets",
-  //       expiration: {
-  //         maxEntries: 32,
-  //         maxAgeSeconds: 24 * 60 * 60, // 24 hours
-  //       },
-  //     },
-  //   },
-
   {
     urlPattern: /\.(?:js)$/i,
     handler: "StaleWhileRevalidate",
@@ -137,31 +115,6 @@ const defaultSwCache = [
     },
   },
 
-  // {
-  //   urlPattern: ({ url }) => {
-  //     const isSameOrigin = self.origin === url.origin;
-  //     if (!isSameOrigin) return false;
-  //     const pathname = url.pathname;
-  //     // Exclude /api/auth/callback/* to fix OAuth workflow in Safari without impact other environment
-  //     // Above route is default for next-auth, you may need to change it if your OAuth workflow has a different callback route
-  //     // Issue: https://github.com/shadowwalker/next-pwa/issues/131#issuecomment-821894809
-  //     if (pathname.startsWith("/api/auth/")) return false;
-  //     if (pathname.startsWith("/api/")) return true;
-  //     return false;
-  //   },
-
-  //   handler: "NetworkFirst",
-  //   method: "GET",
-  //   options: {
-  //     cacheName: "apis",
-  //     expiration: {
-  //       maxEntries: 16,
-  //       maxAgeSeconds: 24 * 60 * 60, // 24 hours
-  //     },
-  //     networkTimeoutSeconds: 10, // fall back to cache if api does not response within 10 seconds
-  //   },
-  // },
-
   {
     urlPattern: ({ url }) => {
       const isSameOrigin = self.origin === url.origin;
@@ -198,26 +151,69 @@ const defaultSwCache = [
       cacheName: "others-prefetch",
       expiration: {
         maxEntries: 128,
-        maxAgeSeconds: 24 * 60 * 60,
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      },
+    },
+  },
+  {
+    urlPattern:
+      /^https:\/\/data\.ninjakiwi\.com\/btd6\/maps\/map\/[A-Z]{7}\/preview$/i,
+    handler: "CacheFirst",
+    options: {
+      cacheName: "btd6-map-previews",
+      expiration: {
+        maxEntries: 64,
+        maxAgeSeconds: 365 * 24 * 60 * 60,
+      },
+      cacheableResponse: {
+        statuses: [0, 200],
       },
     },
   },
 
-  // {
-  //   urlPattern: ({ url }) => {
-  //     const isSameOrigin = self.origin === url.origin;
-  //     return !isSameOrigin;
-  //   },
-  //   handler: "NetworkFirst",
-  //   options: {
-  //     cacheName: "cross-origin",
-  //     expiration: {
-  //       maxEntries: 32,
-  //       maxAgeSeconds: 60 * 60, // 1 hour
-  //     },
-  //     networkTimeoutSeconds: 10,
-  //   },
-  // },
+  {
+    urlPattern: /^https:\/\/static-api\.nkstatic\.com\/.*/i,
+    handler: "CacheFirst",
+    options: {
+      cacheName: "btd6-static-assets",
+      expiration: {
+        maxEntries: 64,
+        maxAgeSeconds: 365 * 24 * 60 * 60,
+      },
+      cacheableResponse: {
+        statuses: [0, 200],
+      },
+    },
+  },
+
+  {
+    urlPattern: /^https:\/\/mediabtd6maplist\.sarto\.dev\/.*/i,
+    handler: "CacheFirst",
+    options: {
+      cacheName: "maplist-static-images",
+      expiration: {
+        maxEntries: 16,
+        maxAgeSeconds: 365 * 24 * 60 * 60,
+      },
+      cacheableResponse: {
+        statuses: [0, 200],
+      },
+    },
+  },
+
+  {
+    urlPattern: ({ url }) =>
+      process.env.NEXT_PUBLIC_API_URL.startsWith(url.origin),
+    handler: "NetworkFirst",
+    options: {
+      cacheName: "maplist-api",
+      expiration: {
+        maxEntries: 32,
+        maxAgeSeconds: 24 * 60 * 60,
+      },
+      networkTimeoutSeconds: 10,
+    },
+  },
 ];
 
 const withPWA = withPWAInit({
@@ -225,75 +221,7 @@ const withPWA = withPWAInit({
   disable: process.env.NODE_ENV === "development",
   extendDefaultRuntimeCaching: false,
   reloadOnOnline: false,
-  workboxOptions: {
-    runtimeCaching: [
-      ...defaultSwCache,
-
-      {
-        urlPattern:
-          /^https:\/\/data\.ninjakiwi\.com\/btd6\/maps\/map\/[A-Z]{7}\/preview$/i,
-        handler: "CacheFirst",
-        options: {
-          cacheName: "btd6-map-previews",
-          expiration: {
-            maxEntries: 64,
-            maxAgeSeconds: 365 * 24 * 60 * 60,
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-
-      {
-        urlPattern: /^https:\/\/static-api\.nkstatic\.com\/.*/i,
-        handler: "CacheFirst",
-        options: {
-          cacheName: "btd6-static-assets",
-          expiration: {
-            maxEntries: 64,
-            maxAgeSeconds: 365 * 24 * 60 * 60,
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-
-      {
-        urlPattern: /^https:\/\/mediabtd6maplist\.sarto\.dev\/.*/i,
-        handler: "CacheFirst",
-        options: {
-          cacheName: "maplist-static-images",
-          expiration: {
-            maxEntries: 16,
-            maxAgeSeconds: 365 * 24 * 60 * 60,
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-
-      {
-        urlPattern: ({ url }) =>
-          process.env.NEXT_PUBLIC_API_URL.startsWith(url.origin),
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "maplist-api",
-          expiration: {
-            maxEntries: 32,
-            maxAgeSeconds: 60 * 60,
-          },
-          networkTimeoutSeconds: 10,
-        },
-      },
-    ],
-  },
+  workboxOptions: { runtimeCaching },
 });
-
-const nextConfig = {
-  output: "standalone",
-};
 
 export default withPWA(nextConfig);
