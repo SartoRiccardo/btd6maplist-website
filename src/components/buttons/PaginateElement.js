da346fcba2f44bf037b9fe3ce86b679c9eb15e40
SartoRiccardo <rikki.sarto@gmail.com>
2024-11-05 01:22:48 +0100

    feat: Prefetches pagination

--- src/components/buttons/PaginateElement.js
+++ src/components/buttons/PaginateElement.js
@@ -1,5 +1,6 @@
 "use client";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 export default function PaginateElement({
   qname,
@@ -12,21 +13,25 @@ export default function PaginateElement({
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
+  // https://github.com/vercel/next.js/issues/49774#issuecomment-1616138326
+  const urlParams = new URLSearchParams(Array.from(searchParams.entries()));
+  urlParams.set(qname, page - 1);
+  const prevUrl = `${pathname}?${urlParams.toString()}`;
+
+  urlParams.set(qname, page + 1);
+  const nextUrl = `${pathname}?${urlParams.toString()}`;
+
+  useEffect(() => {
+    if (page + 1 <= total) router.prefetch(nextUrl);
+    if (page - 1 > 0) router.prefetch(prevUrl);
+  }, [pathname, qname, page]);
+
   const PageButtons = () => (
     <div className="flex-hcenter">
       <button
         className="btn btn-primary"
         disabled={page <= 1}
-        onClick={() =>
-          router.replace(
-            `${pathname}?` +
-              new URLSearchParams({
-                ...searchParams,
-                [qname]: page - 1,
-              }).toString(),
-            { scroll: false }
-          )
-        }
+        onClick={() => router.replace(prevUrl, { scroll: false })}
         data-cy="btn-paginate-back"
       >
         &laquo;
@@ -37,16 +42,7 @@ export default function PaginateElement({
       <button
         className="btn btn-primary"
         disabled={page >= total}
-        onClick={() =>
-          router.replace(
-            `${pathname}?` +
-              new URLSearchParams({
-                ...searchParams,
-                [qname]: page + 1,
-              }).toString(),
-            { scroll: false }
-          )
-        }
+        onClick={() => router.replace(nextUrl, { scroll: false })}
         data-cy="btn-paginate-next"
       >
         &raquo;
